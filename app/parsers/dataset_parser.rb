1441b31aaa732f652277dffbca5eda86579e222f
Jason Truluck <jason@smartlogic.io>
2015-11-05 13:37:13 -0500

    Add the filter for indicator type
    This parses the csv dataset to get the columns after the 5 original
    columns to use as the indicator filters. It is assumed that anything
    after the first 5 columns will be indicators so that the indicators can
    be added dynamically without the need to update the source code.

--- app/parsers/dataset_parser.rb
+++ app/parsers/dataset_parser.rb
@@ -1,6 +1,7 @@
 require 'date'
 
 class DatasetParser
+  INDICATOR_HEADER_RANGE_START = 5
   attr_reader :data, :countries, :years, :group_filters
 
   def initialize(csv)
@@ -13,22 +14,27 @@ class DatasetParser
   end
 
   def countries
-    @countries ||= data.collect{|row| row[:country]}.uniq
+    @countries ||= filter_items_for(:country)
   end
 
   def years
-    @years ||= data.collect{|row| row[:date]}.uniq
+    @years ||= filter_items_for(:date)
   end
 
   def group_filters
-    @group_filters ||= data.collect{|row| row[:grouping]}.uniq
+    @group_filters ||= filter_items_for(:grouping)
+  end
+
+  def indicators
+    @indicators ||= filters_from_columns(INDICATOR_HEADER_RANGE_START)
   end
 
   def metadata
     {
       countries: countries,
       years: years,
-      group_filters: group_filters
+      group_filters: group_filters,
+      indicators: indicators
     }
   end
 
@@ -41,13 +47,25 @@ class DatasetParser
   def csv_parse_options
     {
       row_sep: :auto,
+      remove_empty_values: false,
+      remove_zero_values: false,
       value_converters: { date: DateConverter }
     }
+  end
+
+  def filter_items_for(type)
+    data.collect{|row| row[type]}.uniq
+  end
+
+  def filters_from_columns(start_index, end_index = nil)
+    keys = data.first.keys
+    end_index = keys.count unless end_index
+    data.first.keys[start_index..end_index]
   end
 end
 
 class DateConverter
   def self.convert(value)
-    Date.strptime(value, '%m/%d/%Y').year
+    Date.strptime(value, '%m/%d/%Y')
   end
 end
