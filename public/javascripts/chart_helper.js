0ae51dfd9b306018293db36e2958f10f72bdf78d
Jason Truluck <jason@smartlogic.io>
2015-11-11 17:25:05 -0500

    Add time based charting
    This addes a checkbox to enable time based charting and functionality to
    manipulate the data set

--- public/javascripts/chart_helper.js
+++ public/javascripts/chart_helper.js
@@ -6,11 +6,17 @@ function getCheckedItems(containerId, type) {
   return checkedItems;
 };
 
+function getSelectedItem(containerId, type) {
+  var selector = $('#dataset_' + type + '_' + containerId);
+  return selector.val();
+};
+
 function validateFilters(containerId, metadata) {
   var chartType = getSelectedItem(containerId, 'chart_types');
   var selectedCountries = getCheckedItems(containerId, 'country');
   var selectedDates = getCheckedItems(containerId, 'year');
 
+  toggleOverTimeOption(containerId, selectedDates);
   disablePieOption(containerId, selectedCountries, selectedDates);
   disableDates(containerId, selectedCountries, metadata);
 };
@@ -48,10 +54,11 @@ function disablePieOption(containerId, countries, dates) {
   }
 };
 
-function getSelectedItem(containerId, type) {
-  var selector = $('#dataset_' + type + '_' + containerId);
-  return selector.val();
-};
+function toggleOverTimeOption(containerId, dates) {
+  var overTimeCheckbox = $(".overtime-check-" + containerId);
+  if(dates.length > 1) { overTimeCheckbox.prop('disabled', ''); }
+  else { overTimeCheckbox.prop('disabled', 'disabled'); }
+}
 
 function filterData(dataSet, type, value) {
   var items = dataSet.filter(function(hsh) {
@@ -82,27 +89,33 @@ function reduceDataSet(data, filters, filterType) {
   return result;
 };
 
-function scopeDataSet(data, scope) {
+function scopeDataSet(data, scope, countries) {
   var scopedData = {};
 
-  data.forEach(function(row) {
-    var scopeKey = row[scope];
-
-    if (scopedData[scopeKey] == null) {
-      scopedData[scopeKey] = [row];
-    } else {
-      scopedData[scopeKey].push(row)
-    }
-  });
-
+  if(scope == 'Category') {
+    countries.forEach(function(country) { scopedData[country] = {}; });
+    data.forEach(function(row) {
+      appendToHash(scopedData[row['Country']], row[scope], row);
+    });
+  } else {
+    data.forEach(function(row) {
+      appendToHash(scopedData, row[scope], row);
+    });
+  }
   return scopedData;
 };
+
+function appendToHash(hsh, key, value) {
+  if (hsh[key] == null || hsh[key] == {}) { hsh[key] = [value]; }
+  else { hsh[key].push(value) }
+  return hsh;
+}
 
 function isArray(obj) {
   return Object.prototype.toString.call(obj) === '[object Array]';
 };
 
-function reduceDataBasedOnSelection(countries, grouping, dates) {
+function reduceDataBasedOnSelection(countries, grouping, dates, overTime) {
   var countryFilter = [];
   var dateFilter = [];
   var groupingFilter = [];
@@ -114,49 +127,117 @@ function reduceDataBasedOnSelection(countries, grouping, dates) {
 
   reducedDataSet = dataIntersection([countryFilter, dateFilter, groupingFilter]);
 
-  return scopeDataSet(reducedDataSet, 'Country');
+  var scopedData;
+
+  if(overTime) {
+    scopedData = scopeDataSet(reducedDataSet, 'Category', countries);
+  } else {
+    scopedData = scopeDataSet(reducedDataSet, 'Country');
+  }
+
+  return scopedData;
 };
 
-function generateSeriesData(chartType, countries, indicator, grouping, dates) {
-  var dataSet = reduceDataBasedOnSelection(countries, grouping, dates);
+function generateSeriesData(chartType, countries, indicator, grouping, dates, overTime) {
+  var dataSet = reduceDataBasedOnSelection(countries, grouping, dates, overTime);
   var series = [];
   var xAxis = [];
 
-  if (chartType == "pie") {
+  if(overTime) {
     for(var key in dataSet) {
-      var data = dataSet[key];
-      var newRow = {};
-      newRow['name'] = key;
-      newRow['data'] = [];
+      var countryData = dataSet[key];
 
-      data.forEach(function(row) {
-        var dataElement = {};
-        xAxis.push(row['Category'])
-        dataElement['name'] = row['Category'];
-        dataElement['y'] = row[indicator];
-        newRow['data'].push(dataElement);
-      });
+      for(var countryKey in countryData) {
+        var data = countryData[countryKey];
+        xAxis = dates;
+        var newRow = {};
+        newRow['name'] = key + ' ' + countryKey;
+        newRow['data'] = [];
 
-      series.push(newRow);
-    };
+        var tmpHsh = {};
+
+        data.forEach(function(row) {
+          dates.forEach(function(date) {
+            if(date == row['Date']) {
+              tmpHsh[date] = row[indicator];
+            } else {
+              if(tmpHsh[date] == null || tmpHsh[date] == undefined) {
+                tmpHsh[date] = null;
+              }
+            }
+          });
+        });
+
+        var country;
+        var category;
+        var nullKeys = Object.keys(tmpHsh).filter(function(key) { return tmpHsh[key] == null });
+        var nullIndexes = [];
+
+        nullKeys.forEach(function(date) {
+          nullIndexes.push(dates.indexOf(date));
+        });
+
+        data.forEach(function(row) {
+          var dataElement = {};
+
+          country = row['Country'];
+          category = row['Category'];
+
+          dataElement['name'] = country + ' ' + category;
+          dataElement['y'] = tmpHsh[row['Date']];
+
+          newRow['data'].push(dataElement);
+        });
+
+        nullIndexes.forEach(function(index) {
+          var dataElement = {};
+
+          dataElement['name'] = country + ' ' + category;
+          dataElement['y'] = null;
+
+          newRow['data'].splice(index, 0, dataElement);
+        });
+
+        series.push(newRow);
+      };
+    }
   } else {
-    for(var key in dataSet) {
-      var data = dataSet[key];
-      var newRow = {};
-      newRow['name'] = key;
-      newRow['data'] = [];
+    if (chartType == "pie") {
+      for(var key in dataSet) {
+        var data = dataSet[key];
+        var newRow = {};
+        newRow['name'] = key;
+        newRow['data'] = [];
 
-      data.forEach(function(row) {
-        var dataElement = {};
-        xAxis.push(row['Category'])
-        dataElement['name'] = row['Country'];
-        dataElement['y'] = row[indicator];
-        newRow['data'].push(dataElement);
-      });
+        data.forEach(function(row) {
+          var dataElement = {};
+          xAxis.push(row['Category'])
+          dataElement['name'] = row['Category'];
+          dataElement['y'] = row[indicator];
+          newRow['data'].push(dataElement);
+        });
 
-      series.push(newRow);
-    };
-  }
+        series.push(newRow);
+      };
+    } else {
+      for(var key in dataSet) {
+        var data = dataSet[key];
+        var newRow = {};
+        newRow['name'] = key;
+        newRow['data'] = [];
+
+        data.forEach(function(row) {
+          var dataElement = {};
+          xAxis.push(row['Category'])
+          dataElement['name'] = row['Country'];
+          dataElement['y'] = row[indicator];
+          newRow['data'].push(dataElement);
+        });
+
+        series.push(newRow);
+      };
+    }
+  };
 
   chartComponents = [xAxis, series];
   return chartComponents;
