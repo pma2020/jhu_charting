0d755088509c093921b9613e3313960750c58989
Jason Truluck <jason@smartlogic.io>
2015-12-07 13:18:24 -0500

    Fix translation

--- public/javascripts/translation.js
+++ public/javascripts/translation.js
@@ -2,12 +2,13 @@ function updateLanguage(containerId) {
   $('.i18nable-button').each(function() { $(this).text(translate($(this).val(), labelText)); });
   $('.i18nable-checkbox').each(function() { $(this).val(translate($(this).data('type'), labelText)); });
   $('.i18nable-label').each(function() { $(this).text(translate($(this).data('type'), labelText)); });
-  $("select.i18nable option").each(function() { $(this).text(translate($(this).text(), labelText)); });
+  $("select.i18nable option").each(function() { $(this).text(translate($(this).val(), labelText)); });
   displayHelpText(containerId);
   generateChart(containerId);
 };
 
 function translate(text, type) {
+  if(text == "") { text = 'Select option'; }
   var language = $('#dataset-language-picker').val();
   var key = keyify(text);
   if(type[key]) { text = type[key][language]; }
