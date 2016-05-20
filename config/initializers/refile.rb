7661f0561f5b94e4623573e9b32ba60012c35c3f
Jason Truluck <jason.truluck@gmail.com>
2016-05-20 12:11:50 -0400

    Use S3 only in production

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,12 +1,14 @@
 # config/initializers/refile.rb
-require "refile/s3"
+if Rails.env.production?
+  require "refile/s3"
 
-aws = {
-  access_key_id: ENV['S3_ACCESS_KEY_ID'],
-  secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-  region: "us-east-1",
-  bucket: "pma2020datasets",
-}
+  aws = {
+    access_key_id: ENV['S3_ACCESS_KEY_ID'],
+    secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+    region: "us-east-1",
+    bucket: "pma2020datasets",
+  }
 
-Refile.cache = Refile::S3.new(prefix: "cache", **aws)
-Refile.store = Refile::S3.new(prefix: "store", **aws)
+  Refile.cache = Refile::S3.new(prefix: "cache", **aws)
+  Refile.store = Refile::S3.new(prefix: "store", **aws)
+end
